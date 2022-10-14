2ba1559dbf9e33b66f1a1223321f992f42c622fb
Maheswara Azel Azel <>
2022-10-14 06:54:15 +0000

    gethistory deletehistory addhistory processoperand functions

--- include/calculator.h
+++ include/calculator.h
@@ -1,15 +1,17 @@
 #pragma once
+#include "string_parser.h"
 #include <list>
 #include <string>
 using namespace std;
 
 class Calculator {
 public:
-    double calculate(string expression, bool addToHistory);
-    list<string> getHistory();
-    void deleteAllHistory();
+  double calculate(string expression, bool addToHistory);
+  list<string> getHistory();
+  void deleteAllHistory();
+
 private:
-    list<string> history;
-    void addHistory(string expression, double result);
-    string processOperand(string operand);
+  list<string> history;
+  void addHistory(string expression, double result);
+  string processOperand(string operand);
 };
--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,24 +1,56 @@
 #include "../include/calculator.h"
+#include <math.h>
 
 double Calculator::calculate(string expression, bool addToHistory) {
-    // Bryan
-    return 0.0;
+  // Bryan
+  return 0.0;
 }
 
 list<string> Calculator::getHistory() {
-    // Abhi
-    return list<string>();
+  // Abhi
+  // wht is this for return list<string>();
+  return history;
 }
 
 void Calculator::deleteAllHistory() {
-    // Abhi
+  // Abhi
+  history.clear();
 }
 
 void Calculator::addHistory(string expression, double result) {
-    // Abhi
+  // Abhi
+  history.push_back(expression + "=" + to_string(result));
 }
 
 string Calculator::processOperand(string operand) {
-    // Abhi
-    return operand;
+  // Abhi
+  if (operand.substr(0, 3) == "sin") {
+    operand =
+        to_string(sin(calculate(operand.substr(4, operand.size() - 5), false)));
+  } else if (operand.substr(0, 3) == "cos") {
+    operand =
+        to_string(cos(calculate(operand.substr(4, operand.size() - 5), false)));
+  } else if (operand.substr(0, 3) == "tan") {
+    operand =
+        to_string(tan(calculate(operand.substr(4, operand.size() - 5), false)));
+  } else if (operand.substr(0, 3) == "log") {
+    operand = to_string(
+        log10(calculate(operand.substr(4, operand.size() - 5), false)));
+  } else if (operand.substr(0, 2) == "ln") {
+    operand =
+        to_string(log(calculate(operand.substr(3, operand.size() - 4), false)));
+  } else if (operand.substr(0, 4) == "sqrt") {
+    operand = to_string(
+        sqrt(calculate(operand.substr(5, operand.size() - 6), false)));
+  } else if (operand.substr(0, 3) == "exp") {
+    int idx = StringParser::findPositionOfCurrentExponentComma(operand);
+    double base = calculate(operand.substr(4, idx - 4), false);
+    double power =
+        calculate(operand.substr(idx + 1, operand.size() - idx - 2), false);
+    operand = to_string(pow(base, power));
+  } else if (operand.front() == '(' && operand.back() == ')') {
+    operand =
+        to_string(calculate(operand.substr(1, operand.size() - 2), false));
+  }
+  return operand;
 }
